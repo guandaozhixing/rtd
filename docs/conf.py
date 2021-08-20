f34dbec0654c12a152ea4639c61a64efa4b8beb4
guandaozhixing <zzscholarx@gmail.com>
2021-08-20 21:51:57 +0800

    test

--- docs/conf.py
+++ docs/conf.py
@@ -45,23 +45,6 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
-html_theme_options = {
-    #'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
-    #'analytics_anonymize_ip': False,
-    'logo_only': True,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -97,6 +80,23 @@ pygments_style = None
 #
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    #'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    #'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
